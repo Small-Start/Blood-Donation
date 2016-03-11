c546c572e5810c07ce63d8cf3c059efd153cb96b
kanishk sharma <kanishksharma3012@gmail.com>
2016-03-12 02:08:29 +0530

    API for search added

--- app/src/main/java/com/example/myhp/bloodbank/Take.java
+++ app/src/main/java/com/example/myhp/bloodbank/Take.java
@@ -9,28 +9,81 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by my hp on 3/2/2016.
  */
  public class Take extends Fragment implements View.OnClickListener,AdapterView.OnItemSelectedListener{
     String result[]={"A+","A-","B+","B-","O+","O-","AB+","AB-"};
-    String bloodgroup;
+    String bloodgroup,username;
  @Nullable
  @Override
  public View onCreateView(LayoutInflater inflater,@Nullable ViewGroup container,@Nullable Bundle savedInstanceState){
 
         View v=inflater.inflate(R.layout.take,container,false);
+     v.findViewById(R.id.bsearch).setOnClickListener(this);
 Spinner sp=(Spinner)v.findViewById(R.id.spinner_take);
      ArrayAdapter<String> arr=new ArrayAdapter<String>(getContext(),R.layout.spinner_item,result);
      sp.setAdapter(arr);
      bloodgroup=result[0];
+     username=getActivity().getSharedPreferences("user",0).getString("username","hi");
         return v;
         }
 
 
     @Override
     public void onClick(View v) {
+
+        String url = "http://bloodbanksys.esy.es/bloodbank/blood.php";
+        // Toast.makeText(this,mLastLocation.getLatitude()+" "+mLastLocation.getLongitude(),Toast.LENGTH_LONG).show();
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(getContext(), response, Toast.LENGTH_LONG).show();
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //You can handle error here if you want
+                        Toast.makeText(getContext(), "its not working", Toast.LENGTH_LONG).show();
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                //Adding parameters to request
+                params.put("username",username);
+                params.put("bloodgroup",bloodgroup);
+
+                //returning parameter
+                return params;
+            }
+        };
+
+        //Adding the string request to the queue
+        RequestQueue requestQueue = Volley.newRequestQueue(getContext());
+        requestQueue.add(stringRequest);
+
+        int socketTimeout = 20000;//30 seconds - change to what you want
+        RetryPolicy policy = new DefaultRetryPolicy(socketTimeout, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
+        stringRequest.setRetryPolicy(policy);
 
     }
 
