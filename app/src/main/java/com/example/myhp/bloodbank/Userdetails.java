23b4c6bb6a17fdb353447a26b7c406763d6a3577
kanishk sharma <kanishksharma3012@gmail.com>
2016-03-16 22:52:51 +0530

    Userdetails class added

--- app/src/main/java/com/example/myhp/bloodbank/Userdetails.java
+++ app/src/main/java/com/example/myhp/bloodbank/Userdetails.java
@@ -1,9 +1,16 @@
 package com.example.myhp.bloodbank;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by my hp on 3/12/2016.
  */
-public class Userdetails {
+public class Userdetails extends AppCompatActivity{
 
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.userdetails);
+    }
 }
