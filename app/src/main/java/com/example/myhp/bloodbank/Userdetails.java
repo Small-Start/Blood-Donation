490187acbe84578ae7cb1790db96f629790f0261
kanishk sharma <kanishksharma3012@gmail.com>
2016-03-17 15:16:29 +0530

    Call added

--- app/src/main/java/com/example/myhp/bloodbank/Userdetails.java
+++ app/src/main/java/com/example/myhp/bloodbank/Userdetails.java
@@ -1,8 +1,12 @@
 package com.example.myhp.bloodbank;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.TextView;
 
 import org.json.JSONArray;
@@ -12,7 +16,7 @@ import org.json.JSONObject;
 /**
  * Created by my hp on 3/12/2016.
  */
-public class Userdetails extends AppCompatActivity{
+public class Userdetails extends AppCompatActivity implements View.OnClickListener{
 String response;
     String bloodgroup;
     String email;
@@ -25,10 +29,11 @@ String response;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.userdetails);
-
-        pd=new ProgressDialog();
-        pd.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-        pd.show();
+Toolbar t=(Toolbar)findViewById(R.id.toolbar_userdetails);
+        setSupportActionBar(t);
+      //  pd=new ProgressDialog(getApplicationContext());
+       // pd.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+       // pd.show();
         response=getIntent().getExtras().getString("response");
         JSONArray jr= null;
         try {
@@ -42,9 +47,27 @@ String response;
         } catch (JSONException e) {
             e.printStackTrace();
         }
-        ((TextView)findViewById(R.id.text_userdetails_contactdetails_email)).setText(email);
+        double km=Double.parseDouble(distance);
+        distance=String.format("%.2f",km);
+                ((TextView) findViewById(R.id.text_userdetails_contactdetails_email)).setText(email);
         ((TextView)findViewById(R.id.text_userdetails_contactdetails_mobile)).setText(pno);
         ((TextView)findViewById(R.id.tv_user_details_name)).setText(name);
         ((TextView)findViewById(R.id.text_userdetails_bloodgroup)).setText(bloodgroup);
+        ((TextView)findViewById(R.id.text_userdetails_distance)).setText(distance);
+      //  pd.hide();
+    findViewById(R.id.button_userdetails_call).setOnClickListener(this);
+        findViewById(R.id.button_userdetails_message).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId()==R.id.button_userdetails_call){
+            Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + pno));
+            startActivity(intent);
+        }
+        else
+        {
+
+        }
     }
 }
